b4a183e9d32b32d9c019b719f295566ee9ce0631
Noskina Olha <noskinaolga1@gmail.com>
2025-01-10 22:56:42 +0100

    Navbar section

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import {FaLeaf} from "react-icons/fa";
+import {MdOutlineShoppingCart} from "react-icons/md";
 
 const NavbarMenu = [
     {
@@ -13,18 +14,13 @@ const NavbarMenu = [
     },
     {
         id: 3,
-        title: 'Services',
-        link: '/services',
+        title: 'Products',
+        link: '/products',
     },
     {
         id: 4,
-        title: 'Portfolio',
-        link: '/portfolio',
-    },
-    {
-        id: 5,
-        title: 'Blog',
-        link: '/blog',
+        title: 'Shop',
+        link: '/shop',
     },
     {
         id: 6,
@@ -36,7 +32,7 @@ const NavbarMenu = [
 function Navbar() {
     return (
         <nav>
-            <div className='container'>
+            <div className='container flex justify-between items-center py-4 md:pt-4'>
                 {/*LOGO SECTION*/}
                 <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
                     <p className='text-primary'>Fruit</p>
@@ -44,9 +40,16 @@ function Navbar() {
                     <FaLeaf className="text-green-500" />
                 </div>
                 {/*MENU SECTION*/}
-                <div>
-                    <ul>
-
+                <div className='hidden md:block'>
+                    <ul className='flex items-center gap-6 text-gray-600'>
+                        {NavbarMenu.map((menu) => (
+                            <li key={menu.id}>
+                                <a className='inline-block py-1 px-3 hover:text-primary font-semibold hover:shadow-[0_3px_-1px_#ef4444]' href={menu.link}>{menu.title}</a>
+                            </li>
+                        ))}
+                        <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200'>
+                            <MdOutlineShoppingCart />
+                        </button>
                     </ul>
                 </div>
             </div>
